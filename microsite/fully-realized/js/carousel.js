88aa8bcf24877e900fcb80fbe324da2d6b12b714
Jayce Bordelon <b.jayce@wustl.edu>
2025-04-15 22:14:48 -0500

    Resetting scroll

--- microsite/fully-realized/js/carousel.js
+++ microsite/fully-realized/js/carousel.js
@@ -4,27 +4,35 @@ document.addEventListener('DOMContentLoaded', () => {
   const prevButton = document.querySelector('.carousel-btn.prev');
   const nextButton = document.querySelector('.carousel-btn.next');
   let index = 0;
+  let autoScrollInterval;
 
   function updateCarousel() {
     const slideWidth = slides[0].clientWidth;
     track.style.transform = `translateX(-${index * slideWidth}px)`;
   }
 
+  function startAutoScroll() {
+    clearInterval(autoScrollInterval);
+    autoScrollInterval = setInterval(() => {
+      index = (index + 1) % slides.length;
+      updateCarousel();
+    }, 5000);
+  }
+
   nextButton.addEventListener('click', () => {
     index = (index + 1) % slides.length;
     updateCarousel();
+    startAutoScroll(); 
   });
 
   prevButton.addEventListener('click', () => {
     index = (index - 1 + slides.length) % slides.length;
     updateCarousel();
+    startAutoScroll(); 
   });
 
   window.addEventListener('resize', updateCarousel);
-  updateCarousel();
 
-  setInterval(() => {
-    index = (index + 1) % slides.length;
-    updateCarousel();
-  }, 5000);
+  updateCarousel();
+  startAutoScroll(); 
 });
